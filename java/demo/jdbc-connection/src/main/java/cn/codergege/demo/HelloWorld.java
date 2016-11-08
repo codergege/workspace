c1972510b6f9454994a7648e01e1c7eb25b5a35c
codergege <codergege@163.com>
2016-11-08 17:45:21 +0800

    20161108174521

--- java/demo/jdbc-connection/src/main/java/cn/codergege/demo/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package cn.codergege.demo;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
--- template/spring-mybatis/src/main/java/cn/codergege/packagename/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.codergege.packagename.dao;
-
-import cn.codergege.packagename.model.User;
-
-public interface UserMapper {
-    int insert(User record);
-
-    int insertSelective(User record);
-}
