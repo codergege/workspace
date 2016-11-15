cdf4356c3380e6e57dd6c33292b1b34cd4476d4d
codergege <codergege@163.com>
2016-11-15 15:40:45 +0800

    20161115154045

---
+++ template/gssm/src/main/java/cn/codergege/packagename/dao/DepartmentMapper.java
@@ -0,0 +1,17 @@
+package cn.codergege.packagename.dao;
+
+import cn.codergege.packagename.model.Department;
+
+public interface DepartmentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    Department selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Department record);
+
+    int updateByPrimaryKey(Department record);
+}
---
+++ template/gssm/src/main/java/cn/codergege/packagename/dao/EmployeeMapper.java
@@ -0,0 +1,17 @@
+package cn.codergege.packagename.dao;
+
+import cn.codergege.packagename.model.Employee;
+
+public interface EmployeeMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Employee record);
+
+    int insertSelective(Employee record);
+
+    Employee selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Employee record);
+
+    int updateByPrimaryKey(Employee record);
+}
